82013cf4b10c41e2cfcc5085fd03e15fb0c2efef
Ahmed Musawir <ahmedmusawir@Ahmeds-Mac.local>
2015-10-11 15:49:25 +0800

    3rd Commit: Hopefully Frontpage is done

--- footer.php
+++ footer.php
@@ -1,48 +1,54 @@
-    <section class="footer-callout row">
-    	<div class="footer-callout-text medium-8 column">
-    		CALL US TODAY ON 0409 097 095 OR (08) 8248 6122 FOR MORE INFORMATION ABOUT OUR PROFESSIONAL PLUMBING AND GASFITTING SERVICES!
-    	</div>
-    	<div class="footer-callout-btn medium-4 column">
-    		<a href="#" class="button" title="">CONTACT US</a>
-    	</div>
+    <section class="footer-callout">
+        <div class="row">
+            <div class="footer-callout-text medium-8 column">
+                CALL US TODAY ON 0409 097 095 OR (08) 8248 6122 FOR MORE INFORMATION ABOUT OUR PROFESSIONAL PLUMBING AND GASFITTING SERVICES!
+            </div>
+            <div class="footer-callout-btn medium-4 column">
+                <a href="#" class="button" title="">CONTACT US</a>
+            </div>
+        </div>
     </section>
 
-    <footer class="row">
-    	<div class="footer-widget medium-4 columns">
+    <footer class="the-footer">
 
-    		<h3 class="widget-title">This is a Widget Title</h3>
-    		<ul class="widget no-bullet">
-				<li><a href="#">This is a list</a></li>
-				<li><a href="#">This is a list</a></li>
-				<li><a href="#">This is a list</a></li>
-				<li><a href="#">This is a list</a></li>
-				<li><a href="#">This is a list</a></li>
-			</ul>	
-    	</div>
-    	<div class="footer-widget medium-4 columns">
+    <div class="row">
+            <div class="footer-widget medium-4 columns">
 
-    		<h3 class="widget-title">This is a Widget Title</h3>
-    		<ul class="widget no-bullet">
-				<li><a href="#">This is a list</a></li>
-				<li><a href="#">This is a list</a></li>
-				<li><a href="#">This is a list</a></li>
-				<li><a href="#">This is a list</a></li>
-				<li><a href="#">This is a list</a></li>
-			</ul>	
-    	</div>
-    	<div class="footer-widget medium-4 columns">
-    	
-    		<h3 class="widget-title">This is a Widget Title</h3>
-    		<ul class="widget no-bullet">
-				<li><a href="#">This is a list</a></li>
-				<li><a href="#">This is a list</a></li>
-				<li><a href="#">This is a list</a></li>
-				<li><a href="#">This is a list</a></li>
-				<li><a href="#">This is a list</a></li>
-			</ul>	
-    	</div>
+            <h3 class="widget-title">Quick Links</h3>
+            <ul class="widget no-bullet">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Blocked Drains</a></li>
+                <li><a href="#">Hot water services</a></li>
+                <li><a href="#">Commercial</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>   
+        </div>
+        <div class="footer-widget medium-4 columns">
+
+            <h3 class="widget-title">Contact Information</h3>
+            <p><i class="fa fa-map-marker fa-1x"></i> 611 Military Rd, Largs North SA 5016</p>
+            <p><i class="fa fa-phone fa-1x"></i> 0409 097 095, (08) 8248 6122</p>
+            <p><i class="fa fa-paper-plane fa-1x"></i> office@dciplumbing.com.au</p>
+            
+        </div>
+        <div class="footer-widget medium-4 columns">
+        
+            <h3 class="widget-title">Connect with Us</h3>
+            <ul class="widget no-bullet">
+                <li><a href="#"><i class="fa fa-facebook-official"></i> Follow us on Facebook</a></li>
+                <li><a href="#"><i class="fa fa-twitter-square"></i> Read our Tweets on Twitter</a></li>
+                <li><a href="#"><i class="fa fa-google-plus-square"></i> Join our Google+ circles</a></li>
+                <li><a href="#"><i class="fa fa-linkedin-square"></i> Connect with us on Linkedin</a></li>
+            </ul>   
+        </div>
+    </div>
+ 
     </footer>
 
+<div class="copyright">
+    <p class="text-center">Copyright &copy; 2015 DCI Plumbing & Gas Pty. Ltd. All Right Reserved.</p>
+</div>
 
     <script src="bower_components/jquery/dist/jquery.min.js"></script>
     <!-- // <script type="text/javascript" src="//cdn.jsdelivr.net/jquery.slick/1.5.7/slick.min.js"></script> -->
